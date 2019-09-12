55156a734ae6076e7170b733bc29425c1beb8dc5
amer dardagan <amigo_gagi@outlook.com>
2019-09-12 12:16:29 +0200

    when form is submitted, print: make, year, level + check that all fields have something

--- js/app.js
+++ js/app.js
@@ -1,20 +1,49 @@
 //Variables
-
+const form = document.getElementById ('request-quote'); //AA1/ get Quote Form/Button
 
 
 
 //Event Listeners
-document.addEventListener ('DOMContentLoaded', function (){
+
+eventListeners ();
+
+function eventListeners () {
+
+document.addEventListener ('DOMContentLoaded', function (){ 
     //Create <option> for Years
     const html = new HTMLUI ();
     html.displayYears () //shows the current year
 
+})
+
+//when the form is submitted (AA2)
+form.addEventListener ('submit' , function (e) { 
+    //When i submit the form: print 1.make,2.year
+    e.preventDefault ();
+    const make = document.getElementById ('make').value; //BB1  what continents insurance/car made//1,2, or 3
+    const year = document.getElementById ('year').value; //CC1   what year
+    //Read the radio buttons (I have radio preferance on level of insurance)
+    const level = document.querySelector ('input[name="level"]:checked').value; //DD1 returns what level (BASIC or ADVANCEDis checked
+
+    //check that all fields have something
+    if (make === '' || year === '' || level === '') {
+        console.log ('Error');
+    } else {
+        console.log ('Alright')
+    }
+
+
+
+    console.log (make);
+    console.log (year);
 
 })
 
+}
 
 
-//Objects (Prototypes) 
+
+//Objects+Prototypes) 
 
 //OBJECT-all related to HTML goes here
 function HTMLUI () {}
@@ -25,7 +54,6 @@ HTMLUI.prototype.displayYears = function () {
     const Max = new Date ().getFullYear (), //every year it will be updated 2020/2000/
           Min = Max-20;
 
-    console.log (Min);
 
     //Generate the List with latest 20 years
     const selectYear = document.getElementById ('year');
@@ -38,7 +66,5 @@ HTMLUI.prototype.displayYears = function () {
         selectYear.appendChild (option);
 
     }                 
-
-
-
 }
+
