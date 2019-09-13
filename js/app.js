91895c08b486e1b534ca263797bfc2f2fe689074
amer dardagan <amigo_gagi@outlook.com>
2019-09-13 11:45:08 +0200

    review 1

--- js/app.js
+++ js/app.js
@@ -11,27 +11,27 @@ function eventListeners () {
 
 document.addEventListener ('DOMContentLoaded', function (){ 
     //Create <option> for Years
-    const html = new HTMLUI ();
-    html.displayYears () //shows the current year
+    const html = new HTMLUI (); //enter this after making object
+    html.displayYears () //shows the current year -- enter this after making prototype
 
 })
 
 //when the form is submitted (AA2)
 form.addEventListener ('submit' , function (e) { 
-    //When i submit the form: print 1.make,2.year
+    //When submit the form: print 1.make,2.year
     e.preventDefault ();
     const make = document.getElementById ('make').value; //BB1  what continents insurance/car made//1,2, or 3
     const year = document.getElementById ('year').value; //CC1   what year
     //Read the radio buttons (I have radio preferance on level of insurance)
-    const level = document.querySelector ('input[name="level"]:checked').value; //DD1 returns what level (BASIC or ADVANCEDis checked
+    const level = document.querySelector ('input[name="level"]:checked').value; //DD1 returns what level (BASIC is checked)
 
     //check that all fields have something
     if (make === '' || year === '' || level === '') {
         html.displayError ('All fields are neccessary!')
     } else {
         //make the quotation//FF1
-        const insurance = new Insurance (make, year, level);
-        const price = insurance.calculateQuotation (insurance);
+        const insurance = new Insurance (make, year, level); // enter this later after making object 
+        const price = insurance.calculateQuotation (insurance);// enter this after making prototype
 
     }
     
@@ -50,7 +50,7 @@ function Insurance (make, year, level) {
 
 //Calculate the price for the current quotation
 Insurance.prototype.calculateQuotation = function (insurance) {
-    let price;
+    let price; //important variable
     const base = 2000;
 
     //get make
@@ -75,7 +75,7 @@ Insurance.prototype.calculateQuotation = function (insurance) {
        //get year
 
         const year = insurance.year;
-        const difference = this.getYearDifference (year);
+        const difference = this.getYearDifference (year); //enter after new prototype
 
         //each year cost of insurance will be 3% cheaper (thats why below I needed year difference)
        price = price - ((difference * 3) * price) / 100;  
@@ -87,7 +87,7 @@ Insurance.prototype.calculateQuotation = function (insurance) {
 
 //get the year difference (from this year selected--new prototype
 Insurance.prototype.getYearDifference = function (year) {
-           return new Date().getFullYear () - year;
+           return new Date().getFullYear () - year; //current date - selected date 
 }
 
 
@@ -104,11 +104,11 @@ HTMLUI.prototype.displayYears = function () {
     //Generate the List with latest 20 years
     const selectYear = document.getElementById ('year');
 
-    //Print the values
+    //Print the values/Option
     for (i = Max; i >= Min; i--) {  //not from 0 but begin from  Max; end until and >= Min; i-- jer smanjuje se
         const option = document.createElement ('option');
-        option.value = i //the current year
-        option.textContent = i;
+        option.value = i //the year first printed is i/current year
+        option.textContent = i; //print the current year on page
         selectYear.appendChild (option);
 
     }                 
